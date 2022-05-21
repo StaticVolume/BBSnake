fa8d38ab6f038e46b66b13d767339973b0851672
Filipp <103239257+StaticVolume@users.noreply.github.com>
2022-05-21 19:10:09 +1000

    Delete build-BBSnake-Desktop-Debug directory

--- build-BBSnake-Desktop-Debug/CMakeFiles/BBSnake.dir/point.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/BBSnake.dir/point.cpp.o: \
- /home/frenzy/BBSnake/BBSnake/point.cpp /usr/include/stdc-predef.h \
- /home/frenzy/BBSnake/BBSnake/point.h \
- /home/frenzy/BBSnake/BBSnake/direction.h \
- /home/frenzy/BBSnake/BBSnake/color.h
